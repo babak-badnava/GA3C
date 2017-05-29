b147d5c6cafbc8fad2e2efb7c4e201da6e3c1e61
He <hma02@users.noreply.github.com>
2017-05-29 17:11:37 -0400

    testing with NA=32,NT=4,NP=2 Small DNN (Figure5)

--- ga3c/Config.py
+++ ga3c/Config.py
@@ -50,13 +50,13 @@ class Config:
     # Number of Predictors
     PREDICTORS = 2
     # Number of Trainers
-    TRAINERS = 2
+    TRAINERS = 4
 
     # Device
     DEVICE = 'gpu:0'
 
     # Enable the dynamic adjustment (+ waiting time to start it)
-    DYNAMIC_SETTINGS = True
+    DYNAMIC_SETTINGS = False
     DYNAMIC_SETTINGS_STEP_WAIT = 20
     DYNAMIC_SETTINGS_INITIAL_WAIT = 10
 
